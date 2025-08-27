58d96196b58f99a5c83df56bc3dcd08e6b0dba9f
mathfreak64 <ivanop746@gmail.com>
2025-08-27 11:30:13 +0200

    Worked on the Problem, changed the Program.cs

--- Projekt/NowBoard/NowBoard/Program.cs
+++ Projekt/NowBoard/NowBoard/Program.cs
@@ -1,10 +1,34 @@
+using NowBoard;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services
+// Add services to the container
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
-// Register HttpClient using factory pattern (recommended)
-builder.Services.AddHttpClient();
+// Register HttpClient (with your API base URL)
+builder.Services.AddHttpClient("OjpAPI", client =>
+{
+    client.BaseAddress = new Uri("https://api.opentransportdata.swiss/ojp20/");
+});
+
+// Make the named client available as the default HttpClient
+builder.Services.AddScoped(sp =>
+    sp.GetRequiredService<IHttpClientFactory>().CreateClient("OjpAPI"));
 
 var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/App");
+
+app.Run();
