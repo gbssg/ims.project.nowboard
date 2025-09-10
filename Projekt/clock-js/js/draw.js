ff5d181bd09a5567edd6e9bb6d97d9c9c1c91a9c
mathfreak64 <ivanop746@gmail.com>
2025-09-10 10:56:39 +0200

    add modular classes for better structure

---
+++ Projekt/clock-js/js/draw.js
@@ -0,0 +1,124 @@
+// js/class.js
+export class Draw {
+    constructor(ctx, centerX, centerY, radius) {
+        this.ctx = ctx;
+        this.centerX = centerX;
+        this.centerY = centerY;
+        this.radius = radius;
+    }
+
+    hourmark() {
+      const { ctx, centerX, centerY, radius, innerradius } = this;
+        for (let i = 0; i < 12; i++) {
+            const winkel = (i * Math.PI) / 6;
+            const margin = 10;
+            const x1 = centerX + (radius - 50) * Math.cos(winkel);
+            const y1 = centerY + (radius - 50) * Math.sin(winkel);
+            const x2 = centerX + (radius - margin) * Math.cos(winkel);
+            const y2 = centerY + (radius - margin) * Math.sin(winkel);
+
+            ctx.beginPath();
+            ctx.moveTo(x1, y1);
+            ctx.lineTo(x2, y2);
+            ctx.strokeStyle = "black";
+            ctx.lineWidth = 12;
+            ctx.stroke();
+            ctx.closePath();
+        }
+    }
+
+    minutemark() {
+        const { ctx, centerX, centerY, radius, innerradius } = this;
+        for (let i = 0; i < 60; i++) {
+            const winkel = (i * Math.PI) / 30;
+            const margin = 10;
+            if (i % 5 !== 0) {
+                const x1 = centerX + (radius - 25) * Math.cos(winkel);
+                const y1 = centerY + (radius - 25) * Math.sin(winkel);
+                const x2 = centerX + (radius - margin) * Math.cos(winkel);
+                const y2 = centerY + (radius - margin) * Math.sin(winkel);
+
+                ctx.beginPath();
+                ctx.moveTo(x1, y1);
+                ctx.lineTo(x2, y2);
+                ctx.strokeStyle = "black";
+                ctx.lineWidth = 5;
+                ctx.stroke();
+                ctx.closePath();
+            }
+        }
+    }
+
+    hourpointer(hourwinkel) {
+        const { ctx, centerX, centerY, radius } = this;
+        const hourHandLength = radius * 0.65;
+        // stundenzeiger zeichnen
+      ctx.beginPath();
+      ctx.moveTo(centerX, centerY);
+      ctx.lineTo(
+        centerX + hourHandLength * Math.cos(hourwinkel),
+        centerY + hourHandLength * Math.sin(hourwinkel)
+      );
+      ctx.lineWidth = 15;
+      ctx.strokeStyle = "black";
+      ctx.stroke();
+    }
+
+    minutepointer(minutewinkel) {
+        const { ctx, centerX, centerY, radius } = this;
+        const minuteHandLength = radius * 0.949;
+        // minutenzeiger zeichnen
+      ctx.beginPath();
+      ctx.moveTo(centerX, centerY);
+      ctx.lineTo(
+        centerX + minuteHandLength * Math.cos(minutewinkel),
+        centerY + minuteHandLength * Math.sin(minutewinkel)
+      );
+      ctx.lineWidth = 12;
+      ctx.strokeStyle = "black";
+      ctx.stroke();
+    }
+
+    secondpointer(secondwinkel) {
+        const { ctx, centerX, centerY, radius } = this;
+        const secondHandLength = radius * 0.6;
+        // sekundenzeiger zeichnen
+        ctx.beginPath();
+      ctx.moveTo(centerX, centerY);
+      ctx.lineTo(
+        centerX + secondHandLength * Math.cos(secondwinkel),
+        centerY + secondHandLength * Math.sin(secondwinkel)
+      );
+      ctx.lineWidth = 3;
+      ctx.strokeStyle = "#c33334";
+      ctx.stroke();
+
+      // mittelpunkt
+      ctx.beginPath();
+      ctx.arc(centerX, centerY, 5, 0, Math.PI * 2);
+      ctx.fillStyle = "#c33334";
+      ctx.fill();
+      ctx.closePath();
+
+      ctx.beginPath(); // roten kreis am ende des zeigers
+      // Ende des zeigers wird gebraucht, sprich für x = centerX + secondHandLength * cos(secondwinkel)
+      // und für y = centerY + secondHandLength * sin(secondwinkel)
+
+      
+    }
+
+    rand() {
+        const { ctx, centerX, centerY, radius } = this;
+        // Rand zeichnen
+      ctx.beginPath();
+      ctx.arc(centerX, centerY, radius, 0, Math.PI * 2);
+      ctx.fillStyle = "white";
+      ctx.fill();
+
+      ctx.strokeStyle = "silver";
+      ctx.lineWidth = 8;
+      ctx.stroke();
+    }
+
+    
+}
--- Projekt/clock-js/js/main.js
+++ Projekt/clock-js/js/main.js
@@ -1,3 +1,6 @@
+    import { Draw } from './draw.js';
+    import { Get }  from './get.js';
+    
     const canvas = document.getElementById("clock");
     const ctx = canvas.getContext("2d");
 
@@ -6,7 +9,10 @@
     // Uhrzentrum und Radius
     const  centerX = canvas.width / 2;
     const centerY = canvas.height / 2;
-    const radius = 195
+    const radius = 195;    
+
+    const drawer = new Draw(ctx, centerX, centerY, radius);
+    const getter = new Get();
 
     // Funktion zeichnen der uhr
     function drawClock() {
@@ -14,107 +20,29 @@
       ctx.clearRect(0, 0, canvas.width, canvas.height);
 
       // Rand zeichnen
-      ctx.beginPath();
-      ctx.arc(centerX, centerY, radius, 0, Math.PI * 2);
-      ctx.fillStyle = "white";
-      ctx.fill();
+      drawer.rand();
 
-      ctx.strokeStyle = "black";
-      ctx.lineWidth = 5;
-      ctx.stroke();
-
-      // Mittelpunkt
-      ctx.beginPath();
-      ctx.arc(centerX, centerY, 5, 0, Math.PI * 2);
-      ctx.fillStyle = "black";
-      ctx.fill();
-      ctx.closePath();
-
-      // Stundenmarkierungen
-      for (let i = 0; i < 12; i++) {
-        const winkel = (i * Math.PI) / 6; // 30 grad pro stunde
-        const x1 = centerX + (radius - 40) * Math.cos(winkel);
-        const y1 = centerY + (radius -40) * Math.sin(winkel);
-        const x2 = centerX + radius * Math.cos(winkel);
-        const y2 = centerY + radius * Math.sin(winkel);
-
-        ctx.beginPath();
-        ctx.moveTo(x1, y1);
-        ctx.lineTo(x2, y2);
-        ctx.strokeStyle = "black";
-        ctx.lineWidth = 4;
-        ctx.stroke();
-        ctx.closePath();
-      }
-
-      // minutenmarkierungen
-      for (let i = 0; i < 60; i++) {
-        const winkel = (i * Math.PI) / 30; // 30 / 5 = 6 grad pro minute
-        if (i % 5 !== 0) {
-        const x1 = centerX + (radius - 20) * Math.cos(winkel);
-        const y1 = centerY + (radius -20) * Math.sin(winkel);
-        const x2 = centerX + radius * Math.cos(winkel);
-        const y2 = centerY + radius * Math.sin(winkel);
-
-        ctx.beginPath();
-        ctx.moveTo(x1, y1);
-        ctx.lineTo(x2, y2);
-        ctx.strokeStyle = "black";
-        ctx.lineWidth = 4;
-        ctx.stroke();
-        ctx.closePath();
-        }
-      }
+      // stunden- und minutenmarkierungen zeichnen
+      drawer.hourmark();
+      drawer.minutemark();
 
       // aktuelle zeit holen
-      const now = new Date();
-      const hours = (now.getUTCHours() + 2) % 24; // für schweizer zeit utc+2
-      const minutes = now.getUTCMinutes();
-      const seconds = now.getUTCSeconds();
+      const { hours, minutes, seconds } = getter.time();      
 
-      // winkel berechnen, konvertieren in radian für sinus und cosinus funktionen
-      const hourwinkel = (hours * Math.PI) / 6 + (minutes * Math.PI) / 360; // 30 grad pro h + 0.5 grad pro min
-      const minutewinkel = (minutes * Math.PI) / 30 + (seconds * Math.PI) / 1800; // 6 grad pro min + 0.1 grad pro sek
-      const secondwinkel = (seconds * Math.PI) / 30; // 6 grad pro sek
+      // winkel holen
+      const { hourwinkel, minutewinkel, secondwinkel } = getter.winkel();
 
-      // Zeigerlängen
-      const hourHandLength = radius * 0.6;
-      const minuteHandLength = radius * 0.8;
-      const secondHandLength = radius * 0.9;
+      // zeigerlängen holen
+      const { hourHandLength, minuteHandLength, secondHandLength } = getter.zeigerlängen();
 
       // stundenzeiger zeichnen
-      ctx.beginPath();
-      ctx.moveTo(centerX, centerY);
-      ctx.lineTo(
-        centerX + hourHandLength * Math.cos(hourwinkel),
-        centerY + hourHandLength * Math.sin(hourwinkel)
-      );
-      ctx.lineWidth = 6;
-      ctx.strokeStyle = "black";
-      ctx.stroke();
+      drawer.hourpointer(hourwinkel);
 
       // minutenzeiger zeichnen
-      ctx.beginPath();
-      ctx.moveTo(centerX, centerY);
-      ctx.lineTo(
-        centerX + minuteHandLength * Math.cos(minutewinkel),
-        centerY + minuteHandLength * Math.sin(minutewinkel)
-      );
-      ctx.lineWidth = 4;
-      ctx.strokeStyle = "black";
-      ctx.stroke();
+      drawer.minutepointer(minutewinkel);
 
       // sekundenzeiger zeichnen
-      ctx.beginPath();
-      ctx.moveTo(centerX, centerY);
-      ctx.lineTo(
-        centerX + secondHandLength * Math.cos(secondwinkel),
-        centerY + secondHandLength * Math.sin(secondwinkel)
-      );
-      ctx.lineWidth = 2;
-      ctx.strokeStyle = "red";
-      ctx.stroke();
-      ctx.closePath();
+      drawer.secondpointer(secondwinkel);
     }
 
     drawClock();
